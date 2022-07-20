4950cedde22fbe4914841f36c44926a15114eaf1
yash6318 <yash.agrawal.cse21@iitbhu.ac.in>
2022-07-21 05:24:05 +0530

    week4 done

--- models/book.js
+++ models/book.js
@@ -4,5 +4,20 @@ var bookSchema=new mongoose.Schema({
 	/*TODO: DEFINE the following attributes-
     title, genre, author, description, rating (out of 5), mrp, available_copies(instances).
      */
+    title: String,
+    genre: String,
+    author: String,
+    description: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    mrp: Number,
+    available_copies: {
+        type: Number,
+        min: 0,
+    }
+
 })
 module.exports=mongoose.model("Book",bookSchema);
