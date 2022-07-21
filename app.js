3dd516d4920f481ce389f865a671bb22b6dc07fe
akshit5565 <akshit.garg.cer21@itbhu.ac.in>
2022-07-21 12:57:42 +0530

    Week 4

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ var auth = require("./controllers/auth");
 var store = require("./controllers/store");
 var User = require("./models/user");
 var localStrategy = require("passport-local");
+const path = require("path");
+const cors = require("cors");
 //importing the middleware object to use its functions
 var middleware = require("./middleware"); //no need of writing index.js as directory always calls index.js by default
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-
+app.use(cors());
+app.use(express.json());
+app.set('views',path.join(__dirname, 'views'));
 /*  CONFIGURE WITH PASSPORT */
 app.use(
   require("express-session")({
@@ -37,6 +41,10 @@ app.use(function (req, res, next) {
 });
 
 /* TODO: CONNECT MONGOOSE WITH OUR MONGO DB  */
+mongoose.connect("mongodb+srv://hs05june:Hargun@harpreet.c6kkb6q.mongodb.net/Week4",{useNewUrlParser:true,useUnifiedTopology:true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,"Connection error " ));
+db.once('open', function(){console.log('MONGOOSE CONNECTED!!')});
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Library" });
@@ -52,18 +60,18 @@ app.get("/books", store.getAllBooks);
 
 app.get("/book/:id", store.getBook);
 
-app.get("/books/loaned",
+app.get("/books/loaned",middleware.isLoggedIn,
 //TODO: call a function from middleware object to check if logged in (use the middleware object imported)
  store.getLoanedBooks);
 
-app.post("/books/issue", 
+app.post("/books/issue", middleware.isLoggedIn,
 //TODO: call a function from middleware object to check if logged in (use the middleware object imported)
 store.issueBook);
 
 app.post("/books/search-book", store.searchBooks);
 
 /* TODO: WRITE VIEW TO RETURN AN ISSUED BOOK YOURSELF */
-
+app.post("/books/return/:id",middleware.isLoggedIn,store.returnBook);
 /*-----------------AUTH ROUTES
 TODO: Your task is to complete below controllers in controllers/auth.js
 If you need to add any new route add it here and define its controller
@@ -81,5 +89,5 @@ app.post("/register", auth.postRegister);
 app.get("/logout", auth.logout);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}!`);
+  console.log(`http://localhost:${port}`);
 });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,75 @@
+const User = require('../models/user');
+const passport = require('passport');
 var getLogin = (req, res) => {
   //TODO: render login page
+  try{if(req.isAuthenticated()) {
+    res.redirect('/');
+  }else{
+    res.status(200).render('login',{title:"Books | Login"});
+  }}catch(err){
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 var postLogin = (req, res) => {
   // TODO: authenticate using passport
   //On successful authentication, redirect to next page
+
+  try{req.login(new User({username:req.body.username,password:req.body.password}), (err)=>{
+    if(err){
+      res.status(400).send(err);
+    }else{
+      passport.authenticate("local")(req,res,()=>{
+        res.status(200).redirect("/");
+      });
+    }
+  });}catch(err){
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 var logout = (req, res) => {
   // TODO: write code to logout user and redirect back to the page
-};
+    try{req.logout();
+    res.status(200).redirect('/login');}
+    catch(err){
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+
 
 var getRegister = (req, res) => {
   // TODO: render register page
+  try{if(req.isAuthenticated()) {
+    res.redirect('/');
+  }else{
+    res.status(200).render('register',{title:"Books | Register"});
+  }}catch(err){
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 var postRegister = (req, res) => {
   // TODO: Register user to User db using passport
   //On successful authentication, redirect to next page
+  try{User.register({username: req.body.username}, req.body.password,(err)=>{
+    if(!err){
+      passport.authenticate("local")(req,res,()=>{
+        // res.send("User registered");
+        res.status(200).redirect('/');
+      })
+    }
+    else{
+      res.status(400).send(err);
+    }
+  });}catch(err){
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 module.exports = {
