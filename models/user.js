0841507b6ae35269613c68e81f36a02305fd7610
Krishna <krishnamadhwani2@gmail.com>
2022-07-25 11:35:28 +0530

    Week 4 completed

--- models/user.js
+++ models/user.js
@@ -4,10 +4,21 @@ var	passportLocal=require("passport-local-mongoose");
 var userSchema=new mongoose.Schema({
 
 	//TODO: DEFINE USERNAME AND PASSSWORD ATTRIBUTES
+    username:{
+        type:String,
+    },
+    password:{
+        type:String,
+    },
 
 
     loaned_books:[
         //TODO: embed reference to id's of book copies loaned by this particular user in this array
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'BookCopy',
+        
+         },
     ]
 })
 userSchema.plugin(passportLocal);
