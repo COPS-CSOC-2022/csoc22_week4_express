ef0d1c34c3425d0f8c5fd930e56d8f9084596b20
vcvedika <vcvedikachandra@gmail.com>
2022-07-25 17:22:08 +0530

    Tasks done

--- models/bookCopy.js
+++ models/bookCopy.js
@@ -1,10 +1,27 @@
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
+
 //DEFINING THE BOOK  COPIES MODEL
 var bookCopySchema=new mongoose.Schema({
+
 //TODO: DEFINE the following attributes-
+
  book:  //embed reference to id of book of which its a copy
+ {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book'
+ },
  status: //TRUE IF AVAILABLE TO BE ISSUED, ELSE FALSE 
+ {
+    type: Boolean
+ },
  borrow_data: //date when book was borrowed
+ {
+    type: Date
+ },
  borrower: //embed reference to id of user who has borrowed it 
+ {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+ }
 })
 module.exports=mongoose.model("Bookcopy",bookCopySchema);
