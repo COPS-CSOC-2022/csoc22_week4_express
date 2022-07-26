76d1ef8a9e96bde89229ec62d54b5db85e8c8fd2
archit-2003 <pattanaikarchit@gmail.com>
2022-07-26 23:20:39 +0530

    made required changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,67 @@
+var User = require('../models/user')
+const passport = require('passport');
+
 var getLogin = (req, res) => {
-  //TODO: render login page
+  res.render("login", { title: "Books_kakashi" });
 };
 
-var postLogin = (req, res) => {
-  // TODO: authenticate using passport
-  //On successful authentication, redirect to next page
+var postLogin = (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    faiulerFlash: true
+  })(req, res, next);
 };
 
 var logout = (req, res) => {
-  // TODO: write code to logout user and redirect back to the page
+  req.logout();
+  req.flash('success_msg', 'You have been logged out successfully ....');
+  res.redirect('/login');
 };
 
 var getRegister = (req, res) => {
   // TODO: render register page
+  res.render("register", { title: "Books_kakashi" });
 };
 
 var postRegister = (req, res) => {
-  // TODO: Register user to User db using passport
-  //On successful authentication, redirect to next page
+  let errors = [];
+  const { username, email, password, password2 } = req.body;
+  console.log(req.body)
+
+  if (password != password2) {
+    errors.push({ msg: 'The passwords donot match' });
+  }
+  if (password.length < 10) {
+    errors.push({ msg: 'Length of passwrod must be greater than 10 letters' });
+  }
+
+
+  if (errors.length > 0) {
+    console.log(errors)
+    res.render('register', { errors, username, email, password, password2, title: "Kakashi_Library" });
+
+  }
+  else {
+
+    User.findOne({ email: email })
+      .then((user) => {
+        if (user) {
+          errors.push({ msg: 'The email alreadr exits.Please register with another email...' });
+          res.render('register', { errors, username, email, password, password2, title: "Kakashi_Library" });
+          console.log(errors)
+        }
+        else {
+          const newUser = new User({ username, email, password });
+          newUser.save()
+            .then(user => {
+              req.flash('success_msg', 'You are now registered successfully..');
+              res.redirect('/login');
+            })
+            .catch(err => console.log(err));
+        }
+      })
+  }
 };
 
 module.exports = {
--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,23 @@
-var middlewareObj={};
+var middlewareObj = {};
 //middleware object to check if logged in
-middlewareObj.isLoggedIn=function(req,res,next){
-	/*
+middlewareObj.LoggedIn = function (req, res, next) {
+    /*
     TODO: Write function to check if user is logged in.
     If user is logged in: Redirect to next page
     else, redirect to login page
     */
-	}
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/login');
+}
+middlewareObj.is_logged_out = function (req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
 
-    module.exports=middlewareObj;
+
+module.exports = middlewareObj;
