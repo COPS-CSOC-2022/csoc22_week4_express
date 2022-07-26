74e389c68e4a4897667d78e420bd5fb463323030
Vaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-07-26 22:09:14 +0530

    Full functionality

--- models/user.js
+++ models/user.js
@@ -1,14 +1,28 @@
-var mongoose=require("mongoose");
-var	passportLocal=require("passport-local-mongoose");
+var mongoose = require("mongoose");
+var	passportLocal = require("passport-local-mongoose");
+const Bookcopy = require("../models/bookCopy")
+
 //DEFINING THE USER MODEL
 var userSchema=new mongoose.Schema({
 
 	//TODO: DEFINE USERNAME AND PASSSWORD ATTRIBUTES
 
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
 
     loaned_books:[
         //TODO: embed reference to id's of book copies loaned by this particular user in this array
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Bookcopy'
+        }
     ]
 })
+
 userSchema.plugin(passportLocal);
 module.exports=mongoose.model("User",userSchema);
