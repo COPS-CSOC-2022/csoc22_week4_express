f8cf8be412dc4feee1485dff0f78a6d7457e26c6
anmolkulshrestha <anmol.kulshreshtha.cse21@itbhu.ac.in>
2022-07-30 19:02:14 +0530

    done

--- models/book.js
+++ models/book.js
@@ -1,8 +1,56 @@
 var mongoose=require("mongoose");
 //DEFINING THE BOOK MODEL
 var bookSchema=new mongoose.Schema({
-	/*TODO: DEFINE the following attributes-
-    title, genre, author, description, rating (out of 5), mrp, available_copies(instances).
-     */
+    title: {
+        type: String,
+        required: [true, 'Title is a required field.'],
+        maxLength: [50, 'Max length allowed is 50, got {VALUE}'],
+        trim: true
+    },
+    genre: {
+        type: String,
+        maxLength: [30, 'Max length allowed is 30, got {VALUE}'],
+        default: undefined,
+        trim: true,
+        required: true
+    },
+   
+    author: {
+        type: String,
+        required: true,
+        default: 'anmol',
+        trim: true,
+        maxLength: [20, 'Max length allowed is 20, got {VALUE}'],
+    },
+    description: {
+        type: String,
+        required: false,
+        default: function () {
+            return `${this.title} | A work by ${this.author}`
+        },
+        maxLength: [500, 'Max length allowed is 500, got {VALUE}'],
+    },
+    rating: {
+        type: mongoose.Schema.Types.Decimal128,
+        min: [0.0, 'Rating cannot be negative.'],
+        max: [5.0, 'Maximum possible rating is 5, got {VALUE}'],
+        get: (r) => { return Number(r).toFixed(1); },
+        required: true,
+        default: 0.0
+    },
+    mrp: {
+        type: mongoose.Schema.Types.Decimal128,
+        min: [0.0, 'MRP of the book cannot be negative.'],
+        get: (r) => { return Number(r).toFixed(1); },
+        required: false
+    },
+    available_copies: {
+        type: Number,
+        min: [0, 'Available copies cannot be negative.'],
+        default: 0,
+        required: true,
+        default: 0
+    }
+
 })
-module.exports=mongoose.model("Book",bookSchema);
+module.exports=mongoose.model("book",bookSchema);
--- models/bookCopy.js
+++ models/bookCopy.js
@@ -1,10 +1,29 @@
 var mongoose=require("mongoose");
 //DEFINING THE BOOK  COPIES MODEL
 var bookCopySchema=new mongoose.Schema({
-//TODO: DEFINE the following attributes-
- book:  //embed reference to id of book of which its a copy
- status: //TRUE IF AVAILABLE TO BE ISSUED, ELSE FALSE 
- borrow_data: //date when book was borrowed
- borrower: //embed reference to id of user who has borrowed it 
-})
-module.exports=mongoose.model("Bookcopy",bookCopySchema);
+  book: {
+    type: mongoose.Schema.Types.ObjectID,
+    ref: 'book',
+    required: [true, 'Book is a required field.']
+},
+status: {
+    type: Boolean,
+    required: true,
+    default: true                                   // True => Available to be issued
+},
+borrow_date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    get: (d) => {
+        return Date(d).split("GMT")[0];
+    }
+},
+borrower: {
+    type: mongoose.Schema.Types.ObjectID,
+    ref: 'user'
+}
+   
+  
+}, { timestamps: true })
+module.exports=mongoose.model("bookcopy",bookCopySchema);
